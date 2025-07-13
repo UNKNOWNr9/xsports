b9ae644c7c6e9eacbcd04bf53b09a7b0327019a7
arshia <arshia21420@email.com>
2025-07-13 21:04:13 +0330

    registerForm errors

--- account_module/urls.py
+++ account_module/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegisterView, ActivateAccount
+from .views import RegisterView, ActivateAccountView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('activate_account/<email_active_code>/', ActivateAccount.as_view(), name='activate'),
+    path('activate_account/<email_active_code>/', ActivateAccountView.as_view(), name='activate'),
 ]
