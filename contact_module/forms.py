416edb0ca182c6338ed2fed5d4a671268e6692ef
ارشیا صمیمی فر <A.Samimifar@tav.local>
2025-07-13 14:02:04 +0330

    contact us done!

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 
 class ContactUsForm(forms.Form):
@@ -13,10 +14,6 @@ class ContactUsForm(forms.Form):
         label='نام و نام خانوادگی',
         min_length=6,
         max_length=50,
-        error_messages={
-            'min_length': 'نام و نام خانوادگی خود را کامل وارد کنید',
-            'max_length': 'نام و نام خانوادگی خود را کامل وارد کنید',
-        },
         widget=forms.TextInput(attrs={
             'class': 'form-clt',
             'placeholder': 'نام شما'
@@ -27,22 +24,21 @@ class ContactUsForm(forms.Form):
         label='تلفن همراه',
         min_length=11,
         max_length=11,
-        error_messages={
-            'min_length': 'تلفن همراه خود را به درستی وارد کنید',
-            'max_length': 'تلفن همراه خود را به درستی وارد کنید',
-        },
         widget=forms.TextInput(attrs={
             'class': 'form-clt',
             'placeholder': 'تلفن همراه شما',
         }),
+        validators=[
+            RegexValidator(
+                regex=r'^09\d{9}$',
+                message='تلفن همراه خود را درست وارد کنید!'
+            )
+        ],
     )
 
     email = forms.EmailField(
         label='ایمیل',
         max_length=254,
-        error_messages={
-            'max_length': 'یک ایمیل معتبر وارد کنید'
-        },
         widget=forms.EmailInput(attrs={
             'class': 'form-clt',
             'placeholder': 'ایمیل شما',
@@ -51,10 +47,6 @@ class ContactUsForm(forms.Form):
 
     message = forms.CharField(
         label='پیام شما',
-        min_length=30,
-        error_messages={
-            'min_length': 'پیام شما خیلی کوتاه است'
-        },
         widget=forms.Textarea(attrs={
             'class': 'col-lg-12',
             'placeholder': 'پیام شما',
@@ -67,5 +59,4 @@ class ContactUsForm(forms.Form):
         widget=forms.Select(attrs={
             'class': 'form-clt',
         }),
-        error_messages={'required': 'موضوع پیام را انتخاب کنید.'},
     )
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,11 +1,36 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
-from django.shortcuts import render
 from .forms import ContactUsForm
+from .models import ContactUs
 
 
 class ContactUsView(View):
     def get(self, request):
         contact_form = ContactUsForm()
+        context = {
+            'contact_form': contact_form
+        }
+        return render(request, 'contact_module/contact.html', context)
+
+    def post(self, request):
+        contact_form = ContactUsForm(request.POST)
+        if contact_form.is_valid():
+            user_full_name = contact_form.cleaned_data.get('full_name')
+            user_email = contact_form.cleaned_data.get('email')
+            user_phone = contact_form.cleaned_data.get('phone')
+            user_subject = contact_form.cleaned_data.get('subject')
+            user_message = contact_form.cleaned_data.get('message')
+            new_user = ContactUs(
+                full_name=user_full_name,
+                email=user_email,
+                phone=user_phone,
+                subject=user_subject,
+                message=user_message,
+            )
+            new_user.save()
+            messages.success(request, 'پیام شما با موفقیت ارسال شد.')
+            return redirect(reverse('contact'))
         context = {
             'contact_form': contact_form
         }
